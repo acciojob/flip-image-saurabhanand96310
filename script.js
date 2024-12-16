7103b48664e2623e166cd5a7f331514a2d05e820
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-16 22:02:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // write js code if required
+const initial = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+const final="https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
+const image=document.getElementById("flip")
+image.addEventListener('mouseover', (e)=>{
+	image.src=final;
+});
+
+image.addEventListener('mouseout', ()=>{
+	image.src=initial;
+})
