5165ca4676c38e6383c1780868e8256fc9499400
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-16 22:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // write js code if required
-const initial = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
-const final="https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
-const image=document.querySelector('.flip')
-image.addEventListener('mouseover', (e)=>{
-	image.src=final;
-});
+// const initial = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+// const final="https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
+// const image=document.querySelector('.flip')
+// image.addEventListener('mouseover', (e)=>{
+// 	image.src=final;
+// });
 
-image.addEventListener('mouseout', ()=>{
-	image.src=initial;
-})
+// image.addEventListener('mouseout', ()=>{
+// 	image.src=initial;
+// })
